88be6f2220224c0468794ebc141668c2213551b1
Barrington-Brook <bbmxkrs@gmail.com>
2022-03-03 15:49:59 +0000

    errors resolved with selenium browser

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,10 +1,7 @@
 from seleniumbase import BaseCase
 from faker import Faker
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 fake = Faker()
-browser = webdriver.Firefox(executable_path="/opt/homebrew/Cellar/geckodriver/0.30.0/bin/geckodriver")
 
 def return_tags_in_xpath(xpath):
   print("list of elements")
@@ -52,8 +49,12 @@ class TestPosts(BaseCase):
     self.assert_text("1 like(s)")
     #return_tags_in_xpath('//*')   # debugging function use with pytest -s flag
     
- class TestCancel(BaseCase):
+class TestCancel(BaseCase):
   def test_cancel_post(self):
+    self.open('http://127.0.0.1:5000/auth/register')
+    self.type('input[name="username"]', "Morgan Sparks")
+    self.type('input[name="password"]', "12345678")
+    self.click('input[value="Register"]')
     self.open('http://127.0.0.1:5000/auth/login')
     self.type('input[name="username"]', "Morgan Sparks")
     self.type('input[name="password"]', "12345678")
